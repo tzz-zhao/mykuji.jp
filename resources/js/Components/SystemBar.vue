1d8e65baad6ba303544ab8b49ad654dc45dba435
tzz-zhao <tzz.zhao@gmail.com>
2023-09-07 13:45:07 +0900

    SystemBar修正

--- resources/js/Components/SystemBar.vue
+++ resources/js/Components/SystemBar.vue
@@ -1,9 +1,9 @@
 <template>
     <v-system-bar height="30">
-        <v-btn class="my-4" size="small" variant="text" color="red-darken-1">
+        <v-btn class="my-4" size="small" variant="text" color="red-darken-1" href="/login">
             Login
         </v-btn>
-        <v-btn class="my-4" size="small" variant="text" color="red-darken-1">
+        <v-btn class="my-4" size="small" variant="text" color="red-darken-1" href="/register">
             Register
         </v-btn>
     </v-system-bar>
